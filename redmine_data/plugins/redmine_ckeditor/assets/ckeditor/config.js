7df1e80fa1db4583a81485d9a0b3ea606991ee2a
choiss7 <choipro@gmail.com>
2024-03-05 09:38:31 +0900

    스마트체 추가

--- redmine_data/plugins/redmine_ckeditor/assets/ckeditor/config.js
+++ redmine_data/plugins/redmine_ckeditor/assets/ckeditor/config.js
@@ -7,4 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+config.font_defaultLabel = 'LG스마트체 Regular';
+config.font_names = 'LG스마트체 Regular/LG스마트체 Regular;굴림/Gulim;돋움/Dotum;바탕/Batang;궁서/Gungsuh;맑은 고딕/Malgun;Arial/arial;Comic Sans MS/comic;Courier New/cour;Georgia/georgia;Lucida Sans/LSANS;Tahoma/tahoma;Times New Roman/times;Trebuchet MS/trebuc;Verdana/verdana;';    // 사용 가능한 폰트 설정
+
+	
 };
